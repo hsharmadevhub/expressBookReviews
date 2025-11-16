7abc3050bc2ba1e32e0ebc1dd074314576d54128
Harsh Sharma <36968863+hsharmadrexel@users.noreply.github.com>
2025-11-16 16:17:19 +0530

    Task 4 - Get all books based on title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,10 +28,10 @@ public_users.get("/isbn/:isbn", function (req, res) {
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-  const matchingBooksByAuthor = Object.values(books).filter(book => book.author.includes(req.params.author));
+  const matchingBooksByAuthor = Object.values(books).filter(book => book.author.toLowerCase().includes(req.params.author.toLowerCase()));
 
   if (matchingBooksByAuthor.length == 0) {
-    return res.status(404).json({ messsage: `No books found for the author ${req.params.author}` });
+    return res.status(404).json({ messsage: `No books found with the author: ${req.params.author}` });
   }
 
   return res.status(200).json(matchingBooksByAuthor);
@@ -39,8 +39,13 @@ public_users.get("/author/:author", function (req, res) {
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const matchingBooksByTitle = Object.values(books).filter(book => book.title.toLowerCase().includes(req.params.title.toLowerCase()));
+
+  if (matchingBooksByTitle.length == 0) {
+    return res.status(404).json({ messsage: `No books found with the title: ${req.params.title}` });
+  }
+
+  return res.status(200).json(matchingBooksByTitle);
 });
 
 //  Get book review
