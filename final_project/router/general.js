5c5d958aa462f3440f0f88d866eceb918f7d921d
Harsh Sharma <36968863+hsharmadrexel@users.noreply.github.com>
2025-11-16 23:35:43 +0530

    Tasks 10, 11, 12, 13 - refactor using promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,42 +19,61 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get("/", function (req, res) {
-  return res.status(200).json(books);
+public_users.get("/", async (req, res) => {
+  Promise.resolve(books)
+    .then(data => {
+      return res.status(200).json(data);
+    })
+    .catch(err => {
+      return res.status(500).json({ error: "Failed to fetch books" });
+    });
 });
 
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
   const isbn = req.params.isbn;
-  const book = books[isbn];
-
-  if (!book) {
-    return res.status(404).json({ message: "Book not found" });
-  }
-
-  return res.status(200).json(book);
+  Promise.resolve(books[isbn])
+    .then(book => {
+      if (!book) {
+        return res.status(404).json({ message: "Book not found" });
+      }
+      return res.status(200).json(book);
+    })
+    .catch(err => {
+      return res.status(500).json({ error: "Failed to fetch book" });
+    });
 });
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
   const matchingBooksByAuthor = Object.values(books).filter(book => book.author.toLowerCase().includes(req.params.author.toLowerCase()));
 
-  if (matchingBooksByAuthor.length == 0) {
-    return res.status(404).json({ messsage: `No books found with the author: ${req.params.author}` });
-  }
-
-  return res.status(200).json(matchingBooksByAuthor);
+  Promise.resolve(matchingBooksByAuthor)
+    .then(matchingBooks => {
+      if (matchingBooks.length == 0) {
+        return res.status(404).json({ messsage: `No books found with the author: ${req.params.author}` });
+      }
+      return res.status(200).json(matchingBooks);
+    })
+    .catch(err => {
+      return res.status(500).json({ error: "Failed to fetch book" });
+    });
 });
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
   const matchingBooksByTitle = Object.values(books).filter(book => book.title.toLowerCase().includes(req.params.title.toLowerCase()));
 
-  if (matchingBooksByTitle.length == 0) {
-    return res.status(404).json({ messsage: `No books found with the title: ${req.params.title}` });
-  }
-
-  return res.status(200).json(matchingBooksByTitle);
+  Promise.resolve(matchingBooksByTitle)
+    .then(matchingBooks => {
+      if (matchingBooks.length == 0) {
+        return res.status(404).json({ messsage: `No books found with the title: ${req.params.title}` });
+      }
+      return res.status(200).json(matchingBooks);
+    })
+    .catch(err => {
+      return res.status(500).json({ error: "Failed to fetch book" });
+    });
 });
 
 //  Get book review
