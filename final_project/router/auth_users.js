991d6a6ac7ef6f05f89453860d6062d061087da2
Harsh Sharma <36968863+hsharmadrexel@users.noreply.github.com>
2025-11-16 21:58:35 +0530

    Task 7 - Login

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,18 +5,39 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const doesUserExist = (username)=>{ //returns boolean
+  return users.some(user => user.username === username);
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+  if (!doesUserExist(username)) {
+    return false;
+  }
+
+  const matchingUser = users.find(user => user.username === username);
+  return matchingUser.password === password;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  if (!req.body.username || !req.body.password) {
+    return res.status(404).json({ message: "Username and password are required" });
+  }
+
+  if (!authenticatedUser(req.body.username, req.body.password)) {
+    return res.status(404).json({ message: "Incorrect username and/or password" });
+  }
+
+  let accessToken = jwt.sign({
+    username: req.body.username
+  }, "fingerprint_customer", { expiresIn: 60 * 60 });
+
+  req.session.authorization = {
+    accessToken,
+    username: req.body.username
+  }
+
+  return res.status(200).json({message: "User successfully logged in"});
 });
 
 // Add a book review
@@ -26,5 +47,5 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
 });
 
 module.exports.authenticated = regd_users;
-module.exports.isValid = isValid;
+module.exports.doesUserExist = doesUserExist;
 module.exports.users = users;
